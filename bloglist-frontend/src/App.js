5ec86393196c39af564a2bfa29a977751600c02c
Muciej <kejdzuuu@gmail.com>
2021-02-26 22:44:28 +0100

    Ex. 5.2

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -15,18 +15,31 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
 
     try {
       const user = await loginService.login({ username, password })
+      window.localStorage.setItem('loggedUser', JSON.stringify(user))
       setUser(user)
-      console.log(user)
       setUsername('')
       setPassword('')
     } catch (exception) {
       console.log('Wrong credentials')
     }
+  }
+
+  const handleLogout = (event) => {
+    window.localStorage.clear()
+    window.location.reload()
   }
 
   if (user === null) {
@@ -66,6 +79,7 @@ const App = () => {
           <p>logged in as {user.name}</p> :
           <p>logged in as {user.username}</p>
         }
+        <button onClick={handleLogout}>logout</button>
       </div>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
