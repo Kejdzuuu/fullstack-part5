1cb5ecf9d373313ac8c0c333efd73be7f297257c
Muciej <kejdzuuu@gmail.com>
2021-03-07 18:34:22 +0100

    Ex. 5.14

--- bloglist-frontend/src/components/Blog.test.js
+++ bloglist-frontend/src/components/Blog.test.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
-test('renders content', () => {
+test('renders title and author only', () => {
   const blog = {
     author: 'J.R.R. Author',
     title: 'It\'s title!',
@@ -21,4 +21,23 @@ test('renders content', () => {
 
   const urlLikesDiv = component.container.querySelector('.blogUrlAndLikes')
   expect(urlLikesDiv).toHaveStyle('display: none')
+})
+
+test('renders url and likes after clicking a button', () => {
+  const blog = {
+    author: 'J.R.R. Author',
+    title: 'It\'s title!',
+    url: 'www.url.eu',
+    user: 'user'
+  }
+
+  const component = render(
+    <Blog blog={blog} />
+  )
+
+  const button = component.getByText('view')
+  fireEvent.click(button)
+
+  const urlLikesDiv = component.container.querySelector('.blogUrlAndLikes')
+  expect(urlLikesDiv).not.toHaveStyle('display: none')
 })
